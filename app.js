8c636a8ee071b51f139a31cdb4b2d39b1bf53803
dragonandballz <comebacknader@hotmail.com>
2019-04-02 12:14:31 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname+'/home.html'))
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
