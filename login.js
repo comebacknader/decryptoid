ffbb7a6bc1fc9b7dcc5aa1bbacb3c9160f3cb116
Nader Carun <comebacknader@gmail.com>
2019-07-09 10:55:17 -0700

    Added login.js file

---
+++ login.js
@@ -0,0 +1,11 @@
+const hn = 'localhost'
+const un = 'nadercarun'
+const pw = 'secret'
+const db = 'decryptoidb'
+const sessionPWD = 'secretkeyboardcat'
+
+exports.hn = hn
+exports.un = un
+exports.pw = pw
+exports.db = db
+exports.sessionPWD = sessionPWD 
